4fab93ca5ef2ed7de42d3d78214bf8884ffd906a
Benedikt Reiser <benedikt.reiser@gmail.com>
2022-10-13 15:23:56 +0200

    fix: update setup.py on release

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+    install_requires=["requests", "backoff"],
     name="portal_client",
     version="1.0.0",
     description="Client to interact with Portal's API for application management",
