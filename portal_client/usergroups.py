7f44d75154044598faa93a6781821eb19a5e2e22
Benedikt Reiser <benedikt.reiser@gmail.com>
2022-10-14 09:37:06 +0200

    feat(usergroups): add / remove users to / from groups via cli

--- portal_client/usergroups.py
+++ portal_client/usergroups.py
@@ -31,6 +31,57 @@ def list_usergroups_cli(args):
     print(json.dumps(user_list_response))
 
 
+def add_users_to_group(group, users):
+    maange_users_within_group_url = urljoin(
+        PORTAL_BACKEND_ENDPOINT, f"/api/groups/{group}/users/"
+    )
+    response = requests.post(
+        maange_users_within_group_url,
+        headers={"Authorization": get_authorization_header()},
+        json={"users": users},
+    )
+
+    if not response.ok:
+        print(response.json())
+
+    return response.ok
+
+
+def add_users_to_group_cli(args):
+    add_user_to_group_response = add_users_to_group(group=args.group, users=args.users)
+    print(
+        "User(s) added to group successfully."
+        if add_user_to_group_response
+        else "Failed adding user(s) to group."
+    )
+
+
+def remove_user_from_group(group, user):
+    maange_users_within_group_url = urljoin(
+        PORTAL_BACKEND_ENDPOINT, f"/api/groups/{group}/users/{user}"
+    )
+    response = requests.delete(
+        maange_users_within_group_url,
+        headers={"Authorization": get_authorization_header()},
+    )
+
+    if not response.ok:
+        print(response.json())
+
+    return response.ok
+
+
+def remove_users_from_group_cli(args):
+    remove_user_from_group_response = remove_user_from_group(
+        group=args.group, user=args.user
+    )
+    print(
+        "User removed from group successfully."
+        if remove_user_from_group_response
+        else "Failed removing user from group."
+    )
+
+
 def configure_user_groups_parser(parser: ArgumentParser):
     usergroup_parser = parser.add_subparsers(
         description="List and manage user groups on Portal"
@@ -65,8 +116,31 @@ def configure_user_groups_parser(parser: ArgumentParser):
 
     usergroup_list_parser.set_defaults(func=list_usergroups_cli)
 
-    # TODO: Add users to groups
-    # POST /api/groups/<id>/users/
-    # {
-    #     "users": []
-    # }
+    usergroup_add_users_parser = usergroup_parser.add_parser(
+        "add-users", help="Adds users to a user group."
+    )
+    usergroup_add_users_parser.add_argument(
+        "group", metavar="GROUP_ID", help="The user group (id) to add users to."
+    )
+    usergroup_add_users_parser.add_argument(
+        "users",
+        nargs="+",
+        metavar="USER_ID",
+        help="IDs of users to be added to the user group.",
+    )
+    usergroup_add_users_parser.set_defaults(func=add_users_to_group_cli)
+
+    usergroup_remove_user_parser = usergroup_parser.add_parser(
+        "remove-user", help="Removes a user from a user group."
+    )
+    usergroup_remove_user_parser.add_argument(
+        "group", metavar="GROUP_ID", help="The user group (id) to remove the user from."
+    )
+    usergroup_remove_user_parser.add_argument(
+        "user",
+        metavar="USER_ID",
+        help="ID of user to be removed from the user group.",
+    )
+    usergroup_remove_user_parser.set_defaults(func=remove_users_from_group_cli)
+
+    return usergroup_parser
